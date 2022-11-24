888e228bc3d1ce8a4942528c8cfa3cbf9f2e390c
Thomas McCann <thomas.paul.mccann@gmail.com>
2022-11-24 18:49:47 +0000

    implemented-reusable-table-and-sortable-table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import { Fragment } from 'react';
+//Fragment allows us to place a simple component to allow us to place
+//key props etc.
 
-function Table({ data }) {
-  const renderedRows = data.map((fruit) => {
-    return (
-      <tr className="border-b" key={fruit.name}>
-        <td className="p-3">{fruit.name}</td>
-        <td className="p-3">
-          <div className={`p-3 m-2 ${fruit.color}`}></div>
+function Table({ data, config, keyFn }) {
+  const renderedHeaders = config.map((column) => {
+    //check to see if header element provided and either render it or render the label
+    if (column.header) {
+      return <Fragment key={column.label}>{column.header()}</Fragment>;
+    }
+    return <th key={column.label}>{column.label}</th>;
+  });
+
+  const renderedRows = data.map((rowData) => {
+    const renderedCells = config.map((column) => {
+      return (
+        <td className="p-2" key={column.label}>
+          {column.render(rowData)}
         </td>
-        <td className="p-3">{fruit.score}</td>
+      );
+    });
+    return (
+      <tr className="border-b" key={keyFn(rowData)}>
+        {renderedCells}
       </tr>
     );
   });
   return (
     <table className="table-auto border-spacing-2">
       <thead>
-        <tr className="border-b-2">
-          <th>Fruit</th>
-          <th>Color</th>
-          <th>Score</th>
-        </tr>
+        <tr className="border-b-2">{renderedHeaders}</tr>
       </thead>
       <tbody>{renderedRows}</tbody>
     </table>
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,4 +1,5 @@
-import Table from '../components/Table';
+//import Table from '../components/Table';
+import SortableTable from '../components/SortableTable';
 
 function TablePage() {
   const data = [
@@ -6,10 +7,38 @@ function TablePage() {
     { name: 'Apple', color: 'bg-red-500', score: 3 },
     { name: 'Bannana', color: 'bg-yellow-500', score: 1 },
     { name: 'Lime', color: 'bg-green-500', score: 4 },
+    { name: 'Cherry', color: 'bg-red-700', score: 2 },
   ];
+
+  const config = [
+    {
+      label: 'Fruits',
+      render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name,
+    },
+    {
+      label: 'Color',
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`} />,
+    },
+    {
+      label: 'Score',
+      render: (fruit) => fruit.score,
+      sortValue: (fruit) => fruit.score,
+    },
+    {
+      label: 'Score Squared',
+      render: (fruit) => fruit.score ** 2,
+      sortValue: (fruit) => fruit.score ** 2,
+    },
+  ];
+
+  const keyFn = (fruit) => {
+    return fruit.name;
+  };
+
   return (
     <div>
-      <Table data={data} />
+      <SortableTable data={data} config={config} keyFn={keyFn} />
     </div>
   );
 }
